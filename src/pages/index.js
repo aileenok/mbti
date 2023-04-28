373bbaed0d85f5e2b670679a000a71e7c9f0f1ca
its.aileenok <aileenok02@gmail.com>
2023-04-28 14:00:45 +0900

    수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,8 +27,8 @@ export default function Home() {
   
 
   return (
-    <div className="min-h-screen bg-black-50 flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-bold text-primary mb-8">당신의 성격을 입력하면 MBTI를 알려드립니다.</h1>
+    <div className="min-h-screen bg-black flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold text-white mb-8">당신의 성격을 입력하면 MBTI를 알려드립니다.</h1>
       <form onSubmit={handleSubmit} className="flex flex-col items-center space-y-4">
         <textarea
           value={inputValue}
@@ -42,13 +42,13 @@ export default function Home() {
       </form>
       {result && (
         <div className="flex flex-col items-center mt-8">
-          <p className="text-4xl font-bold">{result.mbti}</p>
+          <p className="text-4xl font-bold text-white">{result.mbti}</p>
           <div className="flex space-x-2 mt-2">
             <span role="img" aria-label="heart" className="text-2xl">
               {result.emoji}
             </span>
           </div>
-          <p className="text-lg font-medium mt-4">오늘의 운세: {result.fortune}</p>
+          <p className="text-lg font-medium mt-4 text-white">오늘의 운세: {result.fortune}</p>
         </div>
       )}
     </div>
